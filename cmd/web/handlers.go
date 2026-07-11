acae9220f13484f29590775feda425e58b5f64a1
sudip <sudipkandel27@gmail.com>
2026-07-11 16:27:38 +0545

    before database

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,14 +7,13 @@ import (
 	"strconv"
 )
 
-
-func (app *application) home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/"{
-		http.NotFound(w,r)
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
 	}
 
-	files := []string {
+	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
@@ -23,27 +22,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		// app.errorLog.Println(err.Error())
+		// http.Error(w, "Internal Server Error", 500)
+		// return
+		app.serverError(w, err)
 		return
 	}
-	
+
 	err = ts.ExecuteTemplate(w, "base", nil)
-	
+
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
+		// app.errorLog.Println(err.Error())
+		// http.Error(w, "Internal Server Error", 500)
+		// return
+		app.serverError(w, err)
 	}
-	
+
 }
 
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request){
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
-		http.NotFound(w,r)
+		// http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -51,11 +54,12 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request){
 
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		// http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 	}
 
 	w.Write([]byte("Create a new snippet..."))
